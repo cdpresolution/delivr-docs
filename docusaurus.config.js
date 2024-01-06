3157e2e97fb3bc2304e5c014adb481db2bda2139
Mat Silva <matsilva@hey.com>
2024-01-05 19:50:41 -0500

    fix: github edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/cdpresolution/delivr-docs/tree/main/docs/',
+          editUrl: 'https://github.com/cdpresolution/delivr-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
